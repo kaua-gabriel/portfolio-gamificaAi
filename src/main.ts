eb68e260a42175ebc77c0bd880322fee86bb369e
Playdeads <gkaua4380@gmail.com>
2024-06-07 14:31:30 -0300

    terminando a segunda parte

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { Engine, FadeInOut } from "excalibur";
+import { welcomeScene } from "./scenes/welcomeScene";
+import { loader } from "./resources";
+import { historyScene } from "./scenes/historyScene";
+import { gamificationScene } from "./scenes/gamificationScene";
+import { expoScene } from "./scenes/expoScene";
+
+const game = new Engine({
+  width: 1200,
+  height: 800,
+  canvasElementId: "jogo"
+})
+
+game.addScene("bemvindo", new welcomeScene())
+game.addScene("historia", new historyScene())
+game.addScene("gamificacao", new gamificationScene())
+game.addScene("exposicao", new expoScene())
+
+game.start(loader).then(() => {
+  game.goToScene("historia", {
+    // Adiciona transição lenta ao ir para a welcomeScene
+    sourceOut: new FadeInOut({ duration: 1000 })
+  })
+})
---
+++ src/scenes/historyScene.ts
@@ -0,0 +1,91 @@
+import { Actor, Color, Engine, FadeInOut, Keys, Scene, SceneActivationContext, Transition, vec } from "excalibur";
+import { Resources } from "../resources";
+
+export class historyScene extends Scene {
+    // Declaração do elementoTexto
+    elementoTexto?: HTMLElement
+
+    // Método para esmaecer um elemento HTML
+    fadeOutElement(elemento: HTMLElement) {
+        // Pegar opacidade do elemento HTML
+        let opacidade = parseFloat(elemento.style.opacity)
+
+        // Repetir diminuição da opacidade
+        setInterval(() => {
+            // Se elemento ainda está visivel
+            if (opacidade > 0) {
+                // Diminuir a opacidade
+                opacidade = opacidade - 0.1
+    
+                // Atualizar a opacidade do elemento
+                elemento.style.opacity = opacidade.toString()
+            }
+        }, 20)        
+    }
+    
+    // Ao entrar ou sair da cena, utiliza o feito de transição lenta
+    onTransition(direction: "in" | "out"): Transition | undefined {
+        return new FadeInOut({
+            direction: direction,
+            color: Color.Black,
+            duration: 1000
+        })
+    }
+    
+    onInitialize(engine: Engine<any>): void {
+        this.backgroundColor = Color.fromHex("#403f4c")
+
+        // Criar elemento com a descrição da empresa
+        this.elementoTexto = document.createElement("div") as HTMLElement
+        
+        // Definir opacidade do elemento para 1 = visível
+        this.elementoTexto.style.opacity = "1"
+
+        // Inserir elementoTexto no container-game
+        let containerGame = document.querySelector(".container-game") as HTMLElement
+        containerGame.appendChild(this.elementoTexto)
+
+        // Adicionar classe na div criada (elementoTexto)
+        this.elementoTexto.classList.add("sobre-gamifica")
+
+        // Adicionar titulo e paragrafo dentro do conteudo da div
+        this.elementoTexto.innerHTML = `<h2>Sobre o GamificaAi</h2>
+        <p>Nossa empresa cria soluções de gamificação personalizadas para empresas de todos os tamanhos e setores,
+          usando inteligência artificial e design de jogos para desenvolver estratégias interativas que melhoram a
+          experiência do usuário e impulsionam resultados. Acreditamos no poder dos jogos e da tecnologia para engajar
+          equipes, aumentar a produtividade e motivar, adaptando cada projeto às necessidades específicas do cliente,
+          desde programas de treinamento interativo até sistemas de recompensa e engajamento de funcionários.</p>`
+
+        // Adicionar logo vertical
+        // Configurar actor do logo vertical
+        let actorLogoVertical = new Actor({
+            pos: vec(engine.drawWidth - 300, engine.halfDrawHeight)
+        })
+
+        // Carregando a imagem do logo
+        let imagemLogoVertical = Resources.LogoVertical.toSprite()
+        imagemLogoVertical.scale = vec(0.7, 0.7)
+
+        // Adicionar a imagem no actor
+        actorLogoVertical.graphics.add(imagemLogoVertical)
+
+        // Renderizar o actor na cena
+        this.add(actorLogoVertical)
+
+        // Configurar a cena para monitorar o evento de tecla pressionada
+        this.input.keyboard.on("press", (event) => {
+            if (event.key == Keys.Enter) {
+                // Criar transição suave do elemento texto
+                this.fadeOutElement(this.elementoTexto!)
+                
+                // Direcionar para a próxima cena
+                engine.goToScene("gamificacao")
+            }
+        })
+    }
+
+    onDeactivate(context: SceneActivationContext<undefined>): void {
+        // Remover elemento texto da tela
+        this.elementoTexto?.remove()
+    }
+}
